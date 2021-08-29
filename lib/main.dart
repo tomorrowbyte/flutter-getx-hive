07053095e4c70a3253d058d3d1617a5fa3c9d01d
sebghatullah <sebghatullah@empirepixel.com>
2021-08-29 10:18:29 +0430

    Add list todos screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx_hive/controllers/todo_controller.dart';
-import 'package:getx_hive/models/todo.dart';
-import 'package:getx_hive/views/todos_list_screen.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+
+import 'views/welcome_screen.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -13,7 +12,6 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
@@ -22,57 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Text(
-              "Welcome to",
-              style: TextStyle(
-                fontWeight: FontWeight.w300,
-                fontSize: 18,
-              ),
-            ),
-          ),
-          SizedBox(height: 10),
-          Center(
-            child: Text(
-              "Todoey",
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          SizedBox(height: 50),
-          ElevatedButton.icon(
-            onPressed: () {
-              Get.to(() => TodosScreen());
-            },
-            icon: Icon(Icons.add),
-            label: Text("Add Todo"),
-          ),
-          ElevatedButton.icon(
-            onPressed: () {
-              Get.to(() => TodosScreen());
-            },
-            icon: Icon(Icons.task),
-            label: Text("My Todos"),
-          ),
-        ],
-      ),
+      home: WelcomeScreen(),
     );
   }
 }
--- lib/views/welcome_screen.dart
+++ lib/views/welcome_screen.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'add_todo_screen.dart';
+import 'todo_list_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              "Welcome to",
+              style: TextStyle(
+                fontWeight: FontWeight.w300,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Center(
+            child: Text(
+              "Todoey",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 50),
+          ElevatedButton.icon(
+            onPressed: () {
+              Get.to(() => TodosScreen());
+            },
+            icon: Icon(Icons.add),
+            label: Text("Add Todo"),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {
+              Get.to(() => MyTodosScreen());
+            },
+            icon: Icon(Icons.task),
+            label: Text("My Todos"),
+          ),
+        ],
+      ),
+    );
   }
 }
